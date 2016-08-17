87cbce85e5028398355a14bfc7d57123f323f233
forehalo <forehalo@gmail.com>
2016-08-17 16:52:22 +0800

    fix code style

--- app/Http/Routes/admin.php
+++ app/Http/Routes/admin.php
@@ -7,7 +7,6 @@
 */
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['pjax']], function () {
-
     Route::get('/', 'AdminController@index');
     Route::get('/template/{version}', 'AdminController@selectTemplate');
     Route::get('/example/{id}', 'AdminController@index');
