468d62b01e8ad14ad482f294d59fd7c53cc68474
forehalo <forehalo@gmail.com>
2016-08-18 17:17:41 +0800

    add default admin seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'password' => bcrypt('123456'),
+            'email' => 'admin@jlxh.com',
+            'phone' => '18888888888'
+        ]);
+    }
+}
