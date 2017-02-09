9a395a260e3aa1d6a6749a7c4a16f39446e06bd7
forehalo <forehalo@gmail.com>
2017-02-09 16:04:15 +0800

    update permission name

--- app/Http/Controllers/Dashboard/Controller.php
+++ app/Http/Controllers/Dashboard/Controller.php
@@ -53,9 +53,9 @@ class Controller extends Base
 
         $sublinks = [];
 
-        Gate::allows('manage', 'App\Models\Admin') &&
+        Gate::allows('show', 'App\Models\Admin') &&
             array_push($sublinks, ['name' => '管理员管理', 'links' => url('dashboard/admins')]);
-        Gate::allows('manage', 'App\Models\Role') &&
+        Gate::allows('show', 'App\Models\Role') &&
             array_push($sublinks, ['name' => '角色管理', 'links' => url('dashboard/roles')]);
         Gate::allows('show', 'App\Models\Permission') &&
             array_push($sublinks, ['name' => '权限列表', 'links' => url('dashboard/permissions')]);
